3450226523594580ba8eb335f7e703bd85b68e1e
konstantin <konstantin.kamushkin@gmail.com>
2026-01-16 17:35:12 +0300

    Initial Commit

---
+++ DivePROD/DivePRODApp.swift
@@ -0,0 +1,17 @@
+//
+//  DivePRODApp.swift
+//  DivePROD
+//
+//  Created by admin on 16.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct DivePRODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
